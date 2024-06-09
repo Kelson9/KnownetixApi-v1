5cfa949ae8837e9bb50892a96f2a565a7729b717
kelson9 <kelsonngoe@gmail.com>
2024-06-09 13:46:28 +0100

    removed jade configs

--- src/main/java/com/KnowNetix/Distributed/AI/Driven/multi/agent/system/DistributedAiDrivenMultiAgentSystemApplication.java
+++ src/main/java/com/KnowNetix/Distributed/AI/Driven/multi/agent/system/DistributedAiDrivenMultiAgentSystemApplication.java
@@ -1,6 +1,6 @@
 package com.KnowNetix.Distributed.AI.Driven.multi.agent.system;
 
-import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.config.JadeAgentManager;
+//import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.config.JadeAgentManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,29 +9,29 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DistributedAiDrivenMultiAgentSystemApplication {
 	public static void main(String[] args) {
 		// Start the JADE platform with the GUI option
-		 startJadePlatformWithGui();
+//		 startJadePlatformWithGui();
 
 		// Start the Spring Boot application
 		ConfigurableApplicationContext context = SpringApplication.run(DistributedAiDrivenMultiAgentSystemApplication.class, args);
 
 		// Initialize the JADE agents
-		JadeAgentManager jadeAgentManager = context.getBean(JadeAgentManager.class);
-		jadeAgentManager.startAgents();
+//		JadeAgentManager jadeAgentManager = context.getBean(JadeAgentManager.class);
+//		jadeAgentManager.startAgents();
 	}
 
-	private static void startJadePlatformWithGui() {
-		try {
-			// Get the JADE runtime instance
-			jade.core.Runtime runtime = jade.core.Runtime.instance();
-
-			// Create a main container with the GUI option
-			jade.core.Profile profile = new jade.core.ProfileImpl(true);
-			jade.wrapper.AgentContainer mainContainer = runtime.createMainContainer(profile);
-
-			// Wait for a while to allow the JADE platform to start
-			Thread.sleep(5000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	private static void startJadePlatformWithGui() {
+//		try {
+//			// Get the JADE runtime instance
+//			jade.core.Runtime runtime = jade.core.Runtime.instance();
+//
+//			// Create a main container with the GUI option
+//			jade.core.Profile profile = new jade.core.ProfileImpl(true);
+//			jade.wrapper.AgentContainer mainContainer = runtime.createMainContainer(profile);
+//
+//			// Wait for a while to allow the JADE platform to start
+//			Thread.sleep(5000);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
--- src/main/java/com/KnowNetix/Distributed/AI/Driven/multi/agent/system/config/JadeAgentManager.java
+++ src/main/java/com/KnowNetix/Distributed/AI/Driven/multi/agent/system/config/JadeAgentManager.java
@@ -1,77 +1,77 @@
-package com.KnowNetix.Distributed.AI.Driven.multi.agent.system.config;
-
-import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.agent.*;
-import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.repository.UserRepository;
-import jade.core.Profile;
-import jade.core.ProfileImpl;
-import jade.core.Runtime;
-import jade.wrapper.AgentContainer;
-import jade.wrapper.AgentController;
-import org.springframework.stereotype.Service;
-
-@Service
-public class JadeAgentManager{
-
-    private AgentContainer mainContainer;
-    private IntelligentTutoringAgent tutoringAgent;
-
-    private RecommenderAgent recommenderAgent;
-
-    private ResourceManagementAgent resourceManagementAgent;
-
-    private UserAgent userAgent;
-
-    private AssessmentAgent assessmentAgent;
-
-    private UserRepository userRepository;
-
-    public void startAgents() {
-        try {
-            // Get the JADE runtime instance
-            Runtime runtime = Runtime.instance();
-
-            // Create a main container
-            Profile profile = new ProfileImpl(true);
-            mainContainer = runtime.createMainContainer(profile);
-
-            // Create the Intelligent Tutoring Agent
-            tutoringAgent = new IntelligentTutoringAgent();
-            AgentController tutoringAgentController = mainContainer.acceptNewAgent("tutoringAgent", tutoringAgent);
-            tutoringAgentController.start();
-
-            recommenderAgent = new RecommenderAgent();
-            AgentController recommenderAgentController = mainContainer.acceptNewAgent("recommenderAgent", recommenderAgent);
-            recommenderAgentController.start();
-
-            resourceManagementAgent = new ResourceManagementAgent();
-            AgentController resourceManagementAgentController = mainContainer.acceptNewAgent("resourceManagementAgent", resourceManagementAgent);
-            resourceManagementAgentController.start();
-
-            userAgent = new UserAgent();
-            AgentController userAgentController = mainContainer.acceptNewAgent("userAgent", userAgent);
-            userAgentController.start();
-
-            // Create the Assessment Agent
-            assessmentAgent = new AssessmentAgent(userRepository);
-            AgentController assessmentAgentController = mainContainer.acceptNewAgent("assessmentAgent", assessmentAgent);
-            assessmentAgentController.start();
-
-            // Wait for a while to allow agents to start and communicate
-            Thread.sleep(5000);
-
-            // Send a message from the Intelligent Tutoring Agent to the Assessment Agent
-//            tutoringAgent.sendMessageToAssessmentAgent("This is a message from the Intelligent Tutoring Agent.");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public IntelligentTutoringAgent getTutoringAgent() {
-        return tutoringAgent;
-    }
-
-//    public AssessmentAgent getAssessmentAgent() {
-//        return assessmentAgent;
+//package com.KnowNetix.Distributed.AI.Driven.multi.agent.system.config;
+//
+//import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.agent.*;
+//import com.KnowNetix.Distributed.AI.Driven.multi.agent.system.repository.UserRepository;
+//import jade.core.Profile;
+//import jade.core.ProfileImpl;
+//import jade.core.Runtime;
+//import jade.wrapper.AgentContainer;
+//import jade.wrapper.AgentController;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class JadeAgentManager{
+//
+//    private AgentContainer mainContainer;
+//    private IntelligentTutoringAgent tutoringAgent;
+//
+//    private RecommenderAgent recommenderAgent;
+//
+//    private ResourceManagementAgent resourceManagementAgent;
+//
+//    private UserAgent userAgent;
+//
+//    private AssessmentAgent assessmentAgent;
+//
+//    private UserRepository userRepository;
+//
+//    public void startAgents() {
+//        try {
+//            // Get the JADE runtime instance
+//            Runtime runtime = Runtime.instance();
+//
+//            // Create a main container
+//            Profile profile = new ProfileImpl(true);
+//            mainContainer = runtime.createMainContainer(profile);
+//
+//            // Create the Intelligent Tutoring Agent
+//            tutoringAgent = new IntelligentTutoringAgent();
+//            AgentController tutoringAgentController = mainContainer.acceptNewAgent("tutoringAgent", tutoringAgent);
+//            tutoringAgentController.start();
+//
+//            recommenderAgent = new RecommenderAgent();
+//            AgentController recommenderAgentController = mainContainer.acceptNewAgent("recommenderAgent", recommenderAgent);
+//            recommenderAgentController.start();
+//
+//            resourceManagementAgent = new ResourceManagementAgent();
+//            AgentController resourceManagementAgentController = mainContainer.acceptNewAgent("resourceManagementAgent", resourceManagementAgent);
+//            resourceManagementAgentController.start();
+//
+//            userAgent = new UserAgent();
+//            AgentController userAgentController = mainContainer.acceptNewAgent("userAgent", userAgent);
+//            userAgentController.start();
+//
+//            // Create the Assessment Agent
+//            assessmentAgent = new AssessmentAgent(userRepository);
+//            AgentController assessmentAgentController = mainContainer.acceptNewAgent("assessmentAgent", assessmentAgent);
+//            assessmentAgentController.start();
+//
+//            // Wait for a while to allow agents to start and communicate
+//            Thread.sleep(5000);
+//
+//            // Send a message from the Intelligent Tutoring Agent to the Assessment Agent
+////            tutoringAgent.sendMessageToAssessmentAgent("This is a message from the Intelligent Tutoring Agent.");
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 //    }
-}
+//
+//    public IntelligentTutoringAgent getTutoringAgent() {
+//        return tutoringAgent;
+//    }
+//
+////    public AssessmentAgent getAssessmentAgent() {
+////        return assessmentAgent;
+////    }
+//}
